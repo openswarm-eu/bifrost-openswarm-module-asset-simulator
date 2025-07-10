7c19c2a547834945386c639f164019d205e250dc
Franz Zeilinger <franz.zeilinger@siemens.com>
2025-07-10 15:18:30 +0200

    UPDATE: add debug logging for processed hooks in asset update logic

--- energy-community-asset-simulator.ts
+++ energy-community-asset-simulator.ts
@@ -261,6 +261,8 @@ const logic = {
         try {
             // update asset values
             if (localStorage[experimentId].numberUpdate == 1){
+                // give as debug information the hook which is processed here
+                context.log.write(`Processing hook ${m.hook[localStorage[experimentId].numberUpdate-1]}`, Log.level.DEBUG)
                 let   wData = {};
                 //  time modulo so day repeats
                 const dataTime = (startAt + simulationAt) % 86400
@@ -377,6 +379,7 @@ const logic = {
             
             // update the grid sensor values
             if (localStorage[experimentId].numberUpdate == 2){
+                context.log.write(`Processing hook ${m.hook[localStorage[experimentId].numberUpdate-1]}`, Log.level.DEBUG)
                 for (const sensorId of localStorage[experimentId].allGridSensors){
                     const sStruct = localStorage[experimentId].byGridSensor[sensorId]
                     
